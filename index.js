f52b1ae81f2c6a90c886ea39b28560eda578fa8b
Colin Gourlay <colin@colin-gourlay.com>
2023-02-13 15:07:04 +1000

    Bit of a refactor. Deliver standard puzzle, as well as Saturday's variety and number puzzles.

--- index.js
+++ index.js
@@ -14,37 +14,80 @@ if (!email || !password) {
 }
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
-const today = new Date();
-const todayNYLocaleString = today.toLocaleString("en-US", {
+const nowUTC = new Date();
+const nowNYLocaleString = nowUTC.toLocaleString("en-US", {
   timeZone: "America/New_York",
 });
-const todayNYDate = new Date(todayNYLocaleString);
-const fileName = `XWD${String(todayNYDate.getMonth() + 1).padStart(
-  2,
-  "0"
-)}${String(todayNYDate.getDate()).padStart(
-  2,
-  "0"
-)}${todayNYDate.getFullYear()}.pdf`;
-const fileURL = `https://s.wsj.net/public/resources/documents/${fileName}`;
-const filePath = join(__dirname, fileName);
+const now = new Date(nowNYLocaleString);
+const weekday = now.getDay();
+const padTwoDigits = (num) => String(num).padStart(2, "0");
+const mdy = [
+  padTwoDigits(now.getMonth() + 1),
+  padTwoDigits(now.getDate()),
+  now.getFullYear(),
+].join("");
 
-(async () => {
-  const download = finished(
+const getUploadFolderId = async (folderPath, token) => {
+  const pathSegments = folderPath.split("/");
+
+  if (pathSegments[0] !== "Document") {
+    pathSegments.unshift("Document");
+  }
+
+  let folderId = undefined;
+
+  for (let pathSegment of pathSegments) {
+    const items = await fileList(token, folderId);
+    const [folder] = items.filter(
+      ({ fileName, isFolder }) => isFolder === "Y" && fileName === pathSegment
+    );
+
+    folderId = folder.id;
+  }
+
+  return folderId;
+};
+
+const getFileURLAndPath = (fileName) => [
+  `https://s.wsj.net/public/resources/documents/${fileName}`,
+  join(__dirname, fileName),
+];
+
+const downloadFile = async (fileURL, filePath) =>
+  finished(
     Readable.fromWeb((await fetch(fileURL)).body).pipe(
       createWriteStream(filePath)
     )
   );
-  const token = await login(email, password);
-  const folders = await fileList(token);
-  const [documentFolder] = folders.filter(
-    ({ fileName }) => fileName === "Document"
-  );
-  const documentFolders = await fileList(token, documentFolder.id);
-  const [crosswordsFolder] = documentFolders.filter(
-    ({ fileName }) => fileName === "Crosswords"
-  );
 
-  await download;
-  await uploadFile(token, filePath, crosswordsFolder.id);
+const deliverFile = async (fileName, folderId, token) => {
+  const [fileURL, filePath] = getFileURLAndPath(fileName);
+
+  await downloadFile(fileURL, filePath);
+  await uploadFile(token, filePath, folderId);
+};
+
+(async () => {
+  if (weekday === 0) {
+    console.log("Today is Sunday. No puzzles to deliver.");
+
+    return;
+  }
+
+  const token = await login(email, password);
+  const folderId = await getUploadFolderId("Document/Crosswords", token);
+  const deliveries = [];
+
+  console.log(`Delivering today's standard puzzle.`);
+  deliveries.push(deliverFile(`XWD${mdy}.pdf`, folderId, token));
+
+  if (weekday === 6) {
+    console.log(`Delivering today's number puzzle.`);
+    deliveries.push(deliverFile(`WSJ_${mdy.slice(0, 4)}.pdf`, folderId, token));
+
+    console.log(`Delivering today's variety puzzle.`);
+    deliveries.push(deliverFile(`SatPuz${mdy}.pdf`, folderId, token));
+  }
+
+  await Promise.all(deliveries);
 })();
